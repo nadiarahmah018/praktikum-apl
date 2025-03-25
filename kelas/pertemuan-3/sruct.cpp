3c95f161214cb44fadb75006aab19af25da899b4
nadiarahmah018 <nadiarahmah888@gmail.com>
2025-03-25 13:58:13 +0800

    Menambahkan file cpp dan exe

---
+++ kelas/pertemuan-3/sruct.cpp
@@ -0,0 +1,28 @@
+/*#include <iostream>
+using namespace std;
+int main() {
+    int pilihan, index;
+    do {
+    system("clear");
+    cout << "Menu Program" << endl;
+    cout << "1. Tampilkan Mahasiswa" << endl;
+    cout << "2. Tambah Mahasiswa" << endl;
+    cout << "3. Ubah Mahasiswa" << endl;
+    cout << "4. Hapus Mahasiswa" << endl;
+    cout << "5. Keluar" << endl;
+    cout << "Pilihan: ";
+    cin >> pilihan;
+    switch (pilihan) {
+        case 1:
+        // Tampilkan Data Mahasiswa
+        break;
+        case 2:
+     // Tambah Data Mahasiswa
+break;
+case 3:
+// Ubah Data Mahasiswa
+break;
+case 4:
+// Hapus Data Mahasiswa
+break;
+*/
